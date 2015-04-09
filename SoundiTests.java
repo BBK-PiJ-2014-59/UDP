2991182b164455d35bbc4a88f2e1791d6ee6daec
J O <folde01@bbk.ac.uk>
2015-04-10 00:09:18 +0100

    A few tests to get a TCP connection going...make them compile and pass now.

--- SoundiTests.java
+++ SoundiTests.java
@@ -1,9 +1,24 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 
-
-
-
 public class SoundiTests { 
+  private SoundiServer server;
+  private SoundiClient client1;
+  private final int firstClientId = 100;
 
+  @Before
+  public void before { 
+    server = new SoundiServerImpl();
+  }
+
+  @Test
+  public void serverListensForTcp { 
+    assertTrue(server.isListeningForTcp());
+  }
+
+  @Test
+  public void clientConnectsWithTcpAndGetsFirstId { 
+    client1 = new SoundiClientImpl();
+    assertEquals(firstClientId, client1.getId());
+  }
 }
