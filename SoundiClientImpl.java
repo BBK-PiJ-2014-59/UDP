48d945b2ee2f1dc86bade6f11822acabbfb53ab4
J O <folde01@bbk.ac.uk>
2015-04-13 11:19:45 +0100

    Need to redo expected exceptions in tests to do with setting up client socket and i/o as they are wrong.

--- SoundiClientImpl.java
+++ SoundiClientImpl.java
@@ -1,8 +1,44 @@
+import java.io.*;
+import java.net.*;
+
 public class SoundiClientImpl implements SoundiClient { 
-  public SoundiClientImpl(int port) { }
-  public SoundiClientImpl() { }
+
+  private String host;
+  private int port;
+
+  private String defaultHost = "localhost";
+  private int defaultPort = "789";
+
+  BufferedReader br;
+  PrintWriter pw;
+  Socket sock;
+  InputStreamReader isr;
+
+  public SoundiClientImpl() {
+    this(defaultHost, defaultPort);
+  }
+
+  public SoundiClientImpl(String host, int port) {
+    this.host = host;
+    this.port = port;
+  }
 
   public void connectTcp() { 
+    try { 
+      sock = new Socket(host, port);
+    } catch (IOException e) { // problem setting up socket 
+      e.printStackTrace();
+    } catch (UnknownHostException e) { // problem with host 
+      e.printStackTrace();
+    }
+
+    try { 
+      isr = new InputStreamReader(sock.getInputStream());
+      br = new BufferedReader(isr);
+      pw = new PrintWriter(sock.getOutPutStream(), true);
+    } catch (IOException e) { 
+      e.printStackTrace();
+    }
   }
 
   public int getId() { 
