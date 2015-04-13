c87ae5cbbe0430b0425249c73b175366549b667d
J O <folde01@bbk.ac.uk>
2015-04-14 00:01:19 +0100

    Got a connection established.

--- SoundServer.java
+++ SoundServer.java
@@ -4,18 +4,26 @@ import java.util.*;
 
 public class SoundServer { 
   
+  private static String serverName = "SoundServer";
   private static int defaultPort = 789;
   private ServerSocket serverSocket;
 
   void start() throws IOException { 
+    log("Creating TCP socket.");
     serverSocket = new ServerSocket(defaultPort); 
+    log("Listening for TCP client.");
     Socket socket = serverSocket.accept();
+    log("Connection with client established.");
     sendClientId(socket);
   }
 
   void sendClientId(Socket s) { 
   }
   
+  public static void log(String s) { 
+    System.out.println(serverName + ": " + s); 
+  }
+
   public static void main(String[] args) { 
     SoundServer soundServer = new SoundServer();
     try { 
